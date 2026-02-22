2dbea74da4bb40f1c97821aebd743f37292ae4a0
Lucas Caldardo Pereira <llucascalldardo@gmail.com>
2026-02-21 23:02:45 -0300

    feat: Cria as rotas do controller com mock

--- src/main/java/dev/java/x/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/dev/java/x/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,4 +1,30 @@
 package dev.java.x.CadastroDeNinjas.Missoes;
 
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missoes")
+
 public class MissoesController {
+
+    @GetMapping("/listar")
+    public String listarMissoes(){
+        return "Missoes listadas";
+    }
+
+    @PostMapping("/criar")
+    public String criarMissoes(){
+        return "Missao criada";
+    }
+
+    @PutMapping("/alterar")
+    public String alterarMissao(){
+        return "Missao alterada com sucesso";
+    }
+
+    @DeleteMapping
+    public String deletarMissao(){
+        return "Missao deletada";
+    }
 }
