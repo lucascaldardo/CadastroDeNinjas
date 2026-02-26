8e6fce1121b1b7a7a85db19feba4bf8b08d2736c
Lucas Caldardo Pereira <llucascalldardo@gmail.com>
2026-02-25 21:25:43 -0300

    feat: Adicona a rota para atualizar ninja

--- src/main/java/dev/java/x/CadastroDeNinjas/Ninjas/NinjaService.java
+++ src/main/java/dev/java/x/CadastroDeNinjas/Ninjas/NinjaService.java
@@ -33,4 +33,11 @@ public class NinjaService {
         ninjaRepository.deleteById(id);
     }
 
+    public NinjaModel atualizarNinja(Long id, NinjaModel ninjaAtualizado){
+        if (ninjaRepository.existsById(id)){
+            ninjaAtualizado.setId(id);
+            return ninjaRepository.save(ninjaAtualizado);
+        }
+        return null;
+    }
 }
