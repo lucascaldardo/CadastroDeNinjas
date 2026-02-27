0b08d71e46207f0b88f89c7a6a3957161665aa2b
Lucas Caldardo Pereira <llucascalldardo@gmail.com>
2026-02-27 20:51:22 -0300

    feat: Inclui DTO como abstracao para entidade na camada de service e controller

--- src/main/java/dev/java/x/CadastroDeNinjas/Ninjas/NinjaService.java
+++ src/main/java/dev/java/x/CadastroDeNinjas/Ninjas/NinjaService.java
@@ -11,13 +11,17 @@ import java.util.Optional;
 public class NinjaService {
 
     private NinjaRepository ninjaRepository;
+    private NinjaMapper ninjaMapper;
 
-    public NinjaService(NinjaRepository ninjaRepository) {
+    public NinjaService(NinjaRepository ninjaRepository, NinjaMapper ninjaMapper) {
         this.ninjaRepository = ninjaRepository;
+        this.ninjaMapper = ninjaMapper;
     }
 
-    public NinjaModel criarNinja(NinjaModel ninja){
-        return ninjaRepository.save(ninja);
+    public NinjaDTO criarNinja(NinjaDTO ninjaDTO){
+        NinjaModel ninja =  ninjaMapper.map(ninjaDTO);
+        ninja = ninjaRepository.save(ninja);
+        return ninjaMapper.map(ninja);
     }
 
     public List<NinjaModel> listarNinjas(){
