57a913b644f27c2a46bda6af4110da8198c365f3
Lucas Caldardo Pereira <llucascalldardo@gmail.com>
2026-02-28 12:33:24 -0300

    refactor: Adiciona final a injecao das dependencias e tambem adiciona o Thymeleaf para renderizacao

--- src/main/java/dev/java/x/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/dev/java/x/CadastroDeNinjas/Missoes/MissoesController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 public class MissoesController {
 
-    private MissoesService missoesService;
+    private final MissoesService missoesService;
 
     public MissoesController(MissoesService missoesService) {
         this.missoesService = missoesService;
